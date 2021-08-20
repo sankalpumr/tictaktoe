ce18bf06866a4db03f383327a0b029cf93384cb4
unknown <sankalp.umr@gmail.com>
2021-08-20 09:52:38 +0530

    Tiktaktoe game

---
+++ tiktaktoe1/src/main/java/com/sankalp/tiktaktoe1/App.java
@@ -0,0 +1,25 @@
+package com.sankalp.tiktaktoe1;
+
+
+public class App 
+{
+   
+      public static void uc1()
+      {
+    	  char[] boardArray = new char[10]; 
+    	  for (char index =1; index<=boardArray.length; index++)
+    	  {
+              boardArray[index]= index++;
+            		  
+    		  System.out.println(" ");      
+              
+    	  }
+    	  
+    	  
+      	public static void main( String[] args )
+    {
+        App.m1 = new m1 App();
+         m1.uc1();
+         
+    }
+}
